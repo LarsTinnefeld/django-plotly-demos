135750fb21b7ddfc920f41440a16b595aa4e752f
Lars <l.tinnefeld@web.de>
2023-12-03 20:23:29 -0500

    Initial commit
    Just getting rolling

--- core/management/commands/populate_database.py
+++ core/management/commands/populate_database.py
@@ -1,8 +1,27 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
+from core.models import PersonSalary
+import csv
 
 
 class Command(BaseCommand):
     help = 'Load data from wage file'
 
     def handle(self, *args, **kwargs):
-        raise NotImplementedError
+        if PersonSalary.objects.count() > 0:
+            PersonSalary.objects.all().delete()
+
+        DATA_FILE = settings.BASE_DIR / 'data' / 'wage.csv'
+
+        with open(DATA_FILE, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            db_rows = []
+            for row in reader:
+                db_rows.append(PersonSalary(
+                    age=row['age'],
+                    education=row['education'],
+                    salary=row['wage'],
+                ))
+
+        # bulk create the records
+        PersonSalary.objects.bulk_create(db_rows, batch_size=1000)
--- scatterplot/settings.py
+++ scatterplot/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
-    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Lars
+# l.tinnefeld@web.de
+# pass123123
