ea3966ccbba9eea223c68fba27d4437bb9c051d1
Lars <l.tinnefeld@web.de>
2023-12-07 20:21:49 -0500

    Final
    Completed project

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Case, When, Value, CharField
 from core.models import PersonSalary
 import plotly.express as px
 
@@ -6,12 +7,13 @@ import plotly.express as px
 
 
 def plot(request):
-    # person_salary = PersonSalary.objects.all()
-    person_salary = PersonSalary.objects.filter(
-        education__in=['1. < HS Grad', '5. Advanced Degree'])
-    ages = person_salary.values_list('age', flat=True)
-    salaries = person_salary.values_list('salary', flat=True)
-    color = person_salary.values_list('education', flat=True)
+    person_salaries = PersonSalary.objects.all()
+    # person_salaries = PersonSalary.objects.filter(
+    #     education__in=['1. < HS Grad', '5. Advanced Degree'])
+
+    ages = person_salaries.values_list('age', flat=True)
+    salaries = person_salaries.values_list('salary', flat=True)
+    color = person_salaries.values_list('education', flat=True)
 
     fig = px.scatter(
         x=ages,
@@ -23,9 +25,25 @@ def plot(request):
     )
     html = fig.to_html()
 
+    # 15-19, 20-24, 25-29, ...
+    YEARS_PER_AGG = 5
+
+    age_bins = [(i, i+YEARS_PER_AGG-1,) for i in range(15, 85, YEARS_PER_AGG)]
+    conditionals = [
+        When(age__range=bin, then=Value(f'{bin}')) for bin in age_bins]
+
+    case = Case(*conditionals, output_field=CharField())
+
+    age_groupings = person_salaries.annotate(
+        age_group=case).values('age_group').order_by('age_group')
+    # print(age_groupings)
+
+    age_groups = age_groupings.values_list('age_group', flat=True)
+    salaries_groups = age_groupings.values_list('salary', flat=True)
+
     fig_box = px.box(
-        x=ages,
-        y=salaries,
+        x=age_groups,
+        y=salaries_groups,
         title='Salary by age',
         height=500,
     )
@@ -38,21 +56,34 @@ def plot(request):
     return render(request, 'scatter.html', context)
 
 
-def boxplot(request):
-    person_salary = PersonSalary.objects.all()
-    ages = person_salary.values_list('age', flat=True)
-    salaries = person_salary.values_list('salary', flat=True)
-    color = person_salary.values_list('education', flat=True)
+# def boxplot(request):
+#     person_salaries = PersonSalary.objects.all()
 
-    fig = px.box(
-        x=ages,
-        y=salaries,
-        title='Salary by age',
-        height=1000,
-    )
-    html = fig.to_html()
+    # 15-19, 20-24, 25-29, ...
+    # YEARS_PER_AGG = 5
 
-    context = {
-        'chart': html,
-    }
-    return render(request, 'scatter.html', context)
+    # age_bins = [(i, i + YEARS_PER_AGG - 1)
+    #             for i in range(15, 85, YEARS_PER_AGG)]
+    # conditionals = [
+    #     When(age_range=bin, then=Value(f'{bin}')) for bin in age_bins]
+
+    # case = Case(*conditionals, output_field=CharField())
+
+    # age_groupings = person_salaries.annotate(age_groupings=case).values
+
+    # ages = person_salaries.values_list('age', flat=True)
+    # salaries = person_salaries.values_list('salary', flat=True)
+    # color = person_salaries.values_list('education', flat=True)
+
+    # fig = px.box(
+    #     x=ages,
+    #     y=salaries,
+    #     title='Salary by age',
+    #     height=1000,
+    # )
+    # html = fig.to_html()
+
+    # context = {
+    #     'chart': html,
+    # }
+    # return render(request, 'scatter.html', context)
